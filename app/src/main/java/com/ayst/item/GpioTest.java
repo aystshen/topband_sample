37d220f07194f6213a0bf51d9311c87f6b667447
shenhb <shenhb@topband.com.cn>
2019-01-23 08:55:30 +0800

    更新GPIO测试，支持GPIO Key Event功能

--- app/src/main/java/com/ayst/item/GpioTest.java
+++ app/src/main/java/com/ayst/item/GpioTest.java
@@ -17,13 +17,68 @@ public class GpioTest {
     }
 
     /**
-     * GPIO control
-     * @param gpio 1,2,3,4
+     * GPIO write
+     * @param gpio 0,1,2,3,4,5,6
      * @param value 0: Low 1: High
      */
-    public void gpioCtrl(int gpio, int value) {
+    public void gpioWrite(int gpio, int value) {
         if (null != mGpioService) {
-            mGpioService.gpioCtrl(gpio, value);
+            mGpioService.gpioWrite(gpio, value);
         }
+    }
+
+    /**
+     * GPIO read
+     * @param gpio 0,1,2,3,4,5,6
+     * @return 0: Low 1: High other：error
+     */
+    public int gpioRead(int gpio) {
+        if (null != mGpioService) {
+            return mGpioService.gpioRead(gpio);
+        }
+        return -1;
+    }
+
+    /**
+     * GPIO direction
+     * @param gpio 0,1,2,3,4,5,6
+     * @param direction 0: input 1: output
+     * @param value 0: Low 1: High
+     */
+    public void gpioDirection(int gpio, int direction, int value) {
+        if (null != mGpioService) {
+            mGpioService.gpioDirection(gpio, direction, value);
+        }
+    }
+
+    /**
+     * GPIO register key event
+     * @param gpio 0,1,2,3,4,5,6
+     */
+    public void gpioRegKeyEvent(int gpio) {
+        if (null != mGpioService) {
+            mGpioService.gpioRegKeyEvent(gpio);
+        }
+    }
+
+    /**
+     * GPIO unregister key event
+     * @param gpio 0,1,2,3,4,5,6
+     */
+    public void gpioUnregKeyEvent(int gpio) {
+        if (null != mGpioService) {
+            mGpioService.gpioUnregKeyEvent(gpio);
+        }
+    }
+
+    /**
+     * Get GPIO number
+     * @return -1: error other: GPIO number
+     */
+    public int gpioGetNumber() {
+        if (null != mGpioService) {
+            return mGpioService.gpioGetNumber();
+        }
+        return -1;
     }
 }
--- app/src/main/java/com/ayst/sample/MainActivity.java
+++ app/src/main/java/com/ayst/sample/MainActivity.java
@@ -1,17 +1,18 @@
 package com.ayst.sample;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 import com.ayst.item.CameraTest;
@@ -25,7 +26,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     private static final String TAG = "Sample";
 
     @BindView(R.id.btn_root_test)
@@ -44,8 +45,29 @@ public class MainActivity extends AppCompatActivity {
     ToggleButton mCameraBtn;
     @BindView(R.id.layout_camera)
     LinearLayout mCameraLayout;
+    @BindView(R.id.spn_gpio)
+    Spinner mGpioSpn;
+    @BindView(R.id.rdo_gpio_input)
+    RadioButton mGpioInputRdo;
+    @BindView(R.id.rdo_gpio_output)
+    RadioButton mGpioOutputRdo;
+    @BindView(R.id.rdo_gpio_key)
+    RadioButton mGpioKeyRdo;
 
     private boolean isSensorActive = false;
+    private int mCurGpio = -1;
+    private static final int[] GPIO_KEY_CODE = {
+            KeyEvent.KEYCODE_GPIO_0,
+            KeyEvent.KEYCODE_GPIO_1,
+            KeyEvent.KEYCODE_GPIO_2,
+            KeyEvent.KEYCODE_GPIO_3,
+            KeyEvent.KEYCODE_GPIO_4,
+            KeyEvent.KEYCODE_GPIO_5,
+            KeyEvent.KEYCODE_GPIO_6,
+            KeyEvent.KEYCODE_GPIO_7,
+            KeyEvent.KEYCODE_GPIO_8,
+            KeyEvent.KEYCODE_GPIO_9};
+
     private GpioTest mGpioTest;
     private CameraTest mCameraTest;
 
@@ -63,12 +85,6 @@ public class MainActivity extends AppCompatActivity {
         mCameraTest = new CameraTest(this, mCameraLayout);
 
         initView();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
-            }
-        }
     }
 
     private void initView() {
@@ -83,15 +99,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mGpioBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                mGpioTest.gpioCtrl(2, b ? 1 : 0);
+        if (mGpioTest.gpioGetNumber() > 0) {
+            String[] spinnerItems = new String[mGpioTest.gpioGetNumber()];
+            for (int i = 0; i < mGpioTest.gpioGetNumber(); i++) {
+                spinnerItems[i] = "GPIO_" + i;
             }
-        });
+            ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
+                    R.layout.spinner_item, spinnerItems);
+            mGpioSpn.setAdapter(spinnerAdapter);
+
+            mCurGpio = mGpioSpn.getSelectedItemPosition();
+            mGpioSpn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    mCurGpio = i;
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+
+            mGpioOutputRdo.setOnCheckedChangeListener(this);
+            mGpioInputRdo.setOnCheckedChangeListener(this);
+            mGpioKeyRdo.setOnCheckedChangeListener(this);
+        } else {
+            mGpioBtn.setEnabled(false);
+        }
     }
 
-    @OnClick({R.id.btn_root_test, R.id.btn_silent_install, R.id.btn_reboot, R.id.btn_start_app})
+    @OnClick({R.id.btn_root_test, R.id.btn_silent_install, R.id.btn_reboot, R.id.btn_start_app, R.id.btn_gpio})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_root_test:
@@ -105,6 +143,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.btn_start_app:
                 SystemAction.startApp(this, "com.android.settings", 5000);
+                break;
+            case R.id.btn_gpio:
+                Log.i(TAG, "onViewClicked, btn_gpio");
+                if (mGpioOutputRdo.isChecked()) {
+                    mGpioTest.gpioWrite(mGpioSpn.getSelectedItemPosition(), mGpioBtn.isChecked() ? 0 : 1);
+                } else {
+                    mGpioBtn.setChecked(!mGpioBtn.isChecked());
+                }
                 break;
         }
     }
@@ -121,6 +167,8 @@ public class MainActivity extends AppCompatActivity {
                     mHandler.postDelayed(mSensorClose, 3000);
                 }
             }
+        } else if (mCurGpio >= 0 && event.getKeyCode() == GPIO_KEY_CODE[mCurGpio]) {
+            mGpioBtn.setChecked(event.getAction() == KeyEvent.ACTION_DOWN);
         }
 
         return super.dispatchKeyEvent(event);
@@ -133,4 +181,23 @@ public class MainActivity extends AppCompatActivity {
             mSensorBtn.setChecked(false);
         }
     };
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b) {
+            switch (compoundButton.getId()) {
+                case R.id.rdo_gpio_input:
+                    mGpioTest.gpioUnregKeyEvent(mCurGpio);
+                    mGpioTest.gpioDirection(mCurGpio, 0, 1);
+                    break;
+                case R.id.rdo_gpio_output:
+                    mGpioTest.gpioUnregKeyEvent(mCurGpio);
+                    mGpioTest.gpioDirection(mCurGpio, 1, 0);
+                    break;
+                case R.id.rdo_gpio_key:
+                    mGpioTest.gpioRegKeyEvent(mCurGpio);
+                    break;
+            }
+        }
+    }
 }
