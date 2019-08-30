898275e15ad941122eff78cf21c8e111aa921ac2
aystshen <ayst_shen@qq.com>
2019-08-30 18:12:08 +0800

    支持主副屏背光调节并保存

--- app/src/main/java/com/ayst/sample/items/backlight/BacklightPresenter.java
+++ app/src/main/java/com/ayst/sample/items/backlight/BacklightPresenter.java
@@ -1,8 +1,7 @@
 package com.ayst.sample.items.backlight;
 
 import android.content.Context;
-
-import com.ayst.sample.items.camera.CameraPresenter;
+import android.provider.Settings;
 
 public class BacklightPresenter {
 
@@ -15,10 +14,31 @@ public class BacklightPresenter {
     }
 
     public void setMainBrightness(int brightness) {
-        mLights.setBrightness(0, brightness);
+        //mLights.setBrightness(0, brightness);
+        saveMainBrightness(brightness);
     }
 
     public void setSubBrightness(int brightness) {
-        mLights.setBrightness(1, brightness);
+        //mLights.setBrightness(1, brightness);
+        saveSubBrightness(brightness);
+    }
+
+    public void saveMainBrightness(int brightness) {
+        try {
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brightness);
+        } catch (Exception localException) {
+            localException.printStackTrace();
+        }
+    }
+
+    public void saveSubBrightness(int brightness) {
+        try {
+            /**
+             * Settings.System.SCREEN_BRIGHTNESS_EXT = "screen_brightness_ext"
+             */
+            Settings.System.putInt(mContext.getContentResolver(), "screen_brightness_ext", brightness);
+        } catch (Exception localException) {
+            localException.printStackTrace();
+        }
     }
 }
