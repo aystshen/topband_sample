b1cf4ff3cae73dc3f3215505665fea18b8985468
aystshen <ayst_shen@qq.com>
2020-05-11 14:20:46 +0800

    监听检查升级结果

--- app/src/main/java/com/ayst/sample/items/upgrade/RomUpgradePresenter.java
+++ app/src/main/java/com/ayst/sample/items/upgrade/RomUpgradePresenter.java
@@ -1,12 +1,16 @@
 package com.ayst.sample.items.upgrade;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.ayst.romupgrade.IRomUpgradeService;
 
@@ -16,8 +20,15 @@ public class RomUpgradePresenter {
     private static final String UPGRADE_PACKAGE_NAME = "com.ayst.romupgrade";
     private static final String ACTION_UPGRADE_SERVICE = "com.ayst.romupgrade.UPGRADE_SERVICE";
 
+    /**
+     * 检查升级结果通知广播Action
+     */
+    public static final String ACTION_CHECK_UPDATE_RESULT =
+            "com.ayst.romupgrade.action.CHECK_UPDATE_RESULT";
+
     private Context mContext;
     private IRomUpgradeService mRomUpgradeService;
+    private BroadcastReceiver mCheckUpdateResultReceiver;
 
     public RomUpgradePresenter(Context context) {
         mContext = context;
@@ -28,10 +39,16 @@ public class RomUpgradePresenter {
         intent.setPackage(UPGRADE_PACKAGE_NAME);
         intent.setAction(ACTION_UPGRADE_SERVICE);
         mContext.bindService(intent, mRomUpgradeServiceConnection, Context.BIND_AUTO_CREATE);
+
+        mCheckUpdateResultReceiver = new CheckUpdateResultReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_CHECK_UPDATE_RESULT);
+        mContext.registerReceiver(mCheckUpdateResultReceiver, filter);
     }
 
     public void stop() {
         mContext.unbindService(mRomUpgradeServiceConnection);
+        mContext.unregisterReceiver(mCheckUpdateResultReceiver);
     }
 
     private ServiceConnection mRomUpgradeServiceConnection = new ServiceConnection() {
@@ -108,6 +125,23 @@ public class RomUpgradePresenter {
                 mRomUpgradeService.deletePackage(packagePath);
             } catch (RemoteException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    class CheckUpdateResultReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "CheckUpdateResultReceiver, action=" + intent.getAction());
+
+            if (TextUtils.equals(intent.getAction(), ACTION_CHECK_UPDATE_RESULT)) {
+                int result = intent.getIntExtra("result", 0);
+                if (result > 0) {
+                    Toast.makeText(context, "Have to upgrade!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(context, "No upgrade!", Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
